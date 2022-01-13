4df005a311cd4218285e4d58431b6b814fe8ef33
Opilane TTHK <opilane@tthk.ee>
2022-01-13 10:57:29 +0200

    commit

---
+++ SnakeOOP/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeOOP
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symb;
+    
+    
+    }
+    public Point(int _x ,int _y , char _symb)
+    {
+
+    }
+
+
+
+
+
+}
---
+++ SnakeOOP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SnakeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 10;
+            char symb = 'a';
+
+            int x1 = 11;
+            int y1 = 10;
+
+            Draw(x, y, symb);
+            Draw(x1, y1, symb);
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+            static void Draw(int x, int y, char symb)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(symb);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
